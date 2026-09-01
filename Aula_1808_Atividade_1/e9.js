06005de16f30b0adaf32914ceda09f37826c7eaa
profRonaldoIFPI <ronaldo.pb@ifpi.edu.br>
2026-09-01 13:31:17 -0300

    Aula de 27/08 - Questões 7 e 9 da atividade

---
+++ Aula_1808_Atividade_1/e9.js
@@ -0,0 +1,13 @@
+let saldoConta = 1000;
+
+console.log(saldoConta);
+saldoConta += 2500; // saldoConta = saldoConta + 2500;
+console.log(saldoConta);
+saldoConta -= 180;
+console.log(saldoConta);
+saldoConta -= 420;
+console.log(saldoConta);
+saldoConta *= 1.05;
+console.log(saldoConta);
+saldoConta /= 2;
+console.log(saldoConta);
---
+++ Aula_1808_Atividade_1/es7.js
@@ -0,0 +1,8 @@
+let num1 = 18;
+let num2 = 4;
+console.log(`Soma:\n\t${num1} + ${num2} = ${num1 + num2}`);
+console.log(`Subtração:\n\t${num1} - ${num2} = ${num1 - num2}`);
+console.log(`Multiplicação:\n\t${num1} x ${num2} = ${num1 * num2}`);
+console.log(`Divisão:\n\t${num1} / ${num2} = ${num1 / num2}`);
+console.log(`Módulo:\n\t${num1} % ${num2} = ${num1 % num2}`);
+console.log(`Potencia:\n\t${num1} ^ ${num2} = ${num1 ** num2}`);
